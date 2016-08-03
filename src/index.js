f50186d1fcf2b71f9a9db8f1fdfc31548ee17612
zhenghe <zhenghe@madadata.com>
2016-08-03 15:00:38 +0800

    add redux-logger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createLogger from 'redux-logger';
+
 import App from './App';
 import toggleDiv from './module/reducer';
 
-const store = createStore(toggleDiv);
+const logger = createLogger();
+
+const store = createStore(toggleDiv, applyMiddleware(logger));
 /* eslint-disable react/jsx-filename-extension */
 ReactDOM.render(
 	<Provider store={store}>
