31124fb8e49c830f22fb34960d4e428697d1a819
zhenghe <zhenghe@madadata.com>
2016-08-04 09:58:35 +0800

    a pureComponentExample

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import React, { Component } from 'react';
+import React, { PureComponent, PropTypes } from 'react';
 
-class App extends Component {
-
-	constructor() {
-		super();
-		this.state = { hidden: false };
-		this.handleButtonClick = this.handleButtonClick.bind(this);
-	}
-
-	getSeeMeJSX() {
-		const { hidden } = this.state;
-		return hidden
-			? null
-			: <div>See Me?</div>;
-	}
-
-	handleButtonClick(e) {
-		e.preventDefault();
-		const { hidden } = this.state;
-		this.setState({ hidden: !hidden });
-	}
-
+class App extends PureComponent {
 	render() {
-		const seeMeJSX = this.getSeeMeJSX();
 		return (
-			<div>
-				<button onClick={this.handleButtonClick}>Click To See Me!</button>
-				{seeMeJSX}
-			</div>
+			<div>{this.props.text}</div>
 		);
 	}
 }
+
+App.propTypes = {
+	text: PropTypes.string,
+};
+
+App.defaultProps = {
+	text: 'hello',
+};
 
 export default App;
